a6c93d09f3ed5b834006e1e7b5236ecdcc62c427
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-20 09:12:38 -0400

    test: unpaired output is empty after filtering
    towards https://github.com/CCBR/CHAMPAGNE/issues/117

--- tests/subworkflows/CCBR/custom/filter_blacklist/custom_test.py
+++ tests/subworkflows/CCBR/custom/filter_blacklist/custom_test.py
@@ -1,1 +1,11 @@
-# TODO https://github.com/CCBR/CHAMPAGNE/issues/117
+import os
+import pathlib
+import pytest
+
+
+@pytest.mark.workflow("filter_blacklist test_filter_blacklist_paired")
+def test_unpaired_is_empty(workflow_dir):
+    unpaired_fastq = pathlib.Path(
+        workflow_dir, "output", "bam", "test.unpaired.fastq.gz"
+    )
+    assert os.stat(unpaired_fastq).st_size == 0
