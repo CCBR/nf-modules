4b400e902a5e2595c1500651dec817d2acf65da3
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-20 16:15:07 -0400

    test: fix bam2fastq path

--- tests/subworkflows/CCBR/filter_blacklist/test_filter_blacklist.py
+++ tests/subworkflows/CCBR/filter_blacklist/test_filter_blacklist.py
@@ -6,6 +6,6 @@ import pytest
 @pytest.mark.workflow("filter_blacklist test_filter_blacklist_paired")
 def test_unpaired_is_empty(workflow_dir):
     unpaired_fastq = pathlib.Path(
-        workflow_dir, "output", "bam", "test.unpaired.fastq.gz"
+        workflow_dir, "output", "bam2fastq", "test.unpaired.fastq.gz"
     )
     assert os.stat(unpaired_fastq).st_size == 0
