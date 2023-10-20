8b9294e2e260d73f1f2f91e259cf61e9a490a0e4
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-20 13:39:00 -0400

    test: simplify test logic

--- tests/modules/CCBR/samtools/filter_aligned/custom_test.py
+++ tests/modules/CCBR/samtools/filter_aligned/custom_test.py
@@ -8,18 +8,12 @@ def test_unaligned_paired(workflow_dir):
     unaligned_bam_file = pysam.AlignmentFile(
         pathlib.Path(workflow_dir, "output/filter/test.unaligned.bam"), "rb"
     )
-    reads_paired = list()
-    reads_unmapped = list()
-    read_mates_unmapped = list()
+    reads_are_good = list()
     for read in unaligned_bam_file:
-        reads_paired.append(read.is_paired)
-        reads_unmapped.append(read.is_unmapped)
-        read_mates_unmapped.append(read.mate_is_unmapped)
-        if not read.is_paired or not read.is_unmappped or not read.mate_is_unmapped:
-            print(
-                f"{read.query_name}; is_paired: {read.is_paired} is_unmapped: {read.is_unmapped} mate_is_unmapped: {read.mate_is_unmapped}"
-            )
-    assert all(reads_paired) and all(reads_unmapped) and all(read_mates_unmapped)
+        reads_are_good.append(
+            read.is_paired and read.is_unmapped and read.mate_is_unmapped
+        )
+    assert all(reads_are_good)
 
 
 @pytest.mark.workflow("samtools filter_aligned test_filter_aligned_single_end")
