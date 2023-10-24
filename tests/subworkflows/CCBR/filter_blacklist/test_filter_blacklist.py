669d6d23690c39e0e3239a1cac2d2171a9ae6dd6
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-24 13:37:20 -0400

    test: that blacklist reads are filtered out

--- tests/subworkflows/CCBR/filter_blacklist/test_filter_blacklist.py
+++ tests/subworkflows/CCBR/filter_blacklist/test_filter_blacklist.py
@@ -1,3 +1,5 @@
+import Bio
+import Bio.SeqIO
 import gzip
 import pathlib
 import pytest
@@ -11,3 +13,31 @@ def test_unpaired_is_empty(workflow_dir):
     with gzip.open(unpaired_fastq, "rt") as infile:
         lines = infile.readlines()
     assert len(lines) == 0
+
+
+def get_record_ids(fastq_path):
+    with gzip.open(fastq_path, "rt") as file_handle:
+        ids = {rec.id.split("/")[0] for rec in Bio.SeqIO.parse(file_handle, "fastq")}
+    return ids
+
+
+def jaccard(set_1, set_2):
+    return len(set_1.intersection(set_2)) / len(set_1.union(set_2))
+
+
+@pytest.mark.workflow("filter_blacklist test_filter_blacklist_paired")
+def test_blacklist_filtered_out(workflow_dir):
+    blacklist = get_record_ids(
+        pathlib.Path(workflow_dir, "output", "blacklist.fastq.gz")
+    )
+    out_1 = get_record_ids(
+        pathlib.Path(workflow_dir, "output", "picard", "test_1.fastq.gz")
+    )
+    out_2 = get_record_ids(
+        pathlib.Path(workflow_dir, "output", "picard", "test_2.fastq.gz")
+    )
+    assert (
+        not blacklist.intersection(out_1)
+        and not blacklist.intersection(out_2)
+        and jaccard(out_1, out_2) == 1
+    )
