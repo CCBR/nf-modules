e140283a725b3b4a92a833af96713b79b4d7ac7c
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-10-26 13:10:35 -0400

    refactor: simplify module quarto template with params
    instead of using python f-strings. now the template code is regular quarto, rather than needing to escape curly braces

--- docs/create-listings.py
+++ docs/create-listings.py
@@ -5,6 +5,7 @@ import shutil
 from ruamel.yaml import YAML
 
 yaml = YAML(typ="rt")
+yaml_str = YAML(typ=["rt", "string"])
 
 
 def main():
@@ -19,33 +20,36 @@ def main():
 def write_listing_components(mtype):
     with open(f"docs/templates/modules.qmd", "r") as infile:
         template = infile.read()
-    listing = [
-        write_qmd(meta, template, mtype=mtype)
-        for meta in get_yaml_globs(f"{mtype}/**/meta.yml")
-    ]
-    write_yaml_object(listing, f"docs/{mtype}/{mtype}.yml")
+    for nf_meta in get_yaml_globs(f"{mtype}/**/meta.yml"):
+        write_qmd(nf_meta, template, mtype=mtype)
 
 
-def write_qmd(meta, template, mtype="modules"):
-    out_yml = f"docs/{mtype}/{meta['name']}.yml"
-    with open(out_yml, "w") as outfile:
-        yaml.dump(meta, outfile)
-    qmd = template.format(
-        title=meta["name"],
-        subtitle=meta["description"],
-        yaml_file=out_yml,
-        module_name="/".join(meta["name"].split("_")),
-        module_type=mtype,
-        module_path=meta["filename"].rstrip("meta.yml"),
-    )
-    out_qmd = f"docs/{mtype}/{meta['name']}.qmd"
-    with open(out_qmd, "w") as outfile:
-        outfile.write(qmd)
-    meta["title"] = meta.pop("name")
-    meta["subtitle"] = meta.pop("description")
-    meta["categories"] = meta.pop("keywords")
-    meta["path"] = f"/docs/{mtype}/{meta['title']}.qmd"
-    return meta
+def write_qmd(nf_meta, template, mtype="modules"):
+    # write out nextflow module meta.yml as-is
+    out_nf_yml = f"docs/{mtype}/{nf_meta['name']}.yml"
+    with open(out_nf_yml, "w") as outfile:
+        yaml.dump(nf_meta, outfile)
+
+    # create metadata for quarto file & listing
+    qmd_dict = {
+        "title": nf_meta["name"],
+        "subtitle": nf_meta["description"],
+        "categories": nf_meta["keywords"],
+        "params": {
+            "nf_yaml_file": out_nf_yml,
+            "module_name": "/".join(nf_meta["name"].split("_")),
+            "module_type": mtype,
+            "module_path": nf_meta["filename"].rstrip("meta.yml"),
+        },
+    }
+    extra_meta = "\n" + yaml_str.dump_to_string(qmd_dict)
+
+    # generate quarto file from template
+    template_split = template.split("---")
+    template_split[1] = "\n".join([extra_meta, template_split[1]])
+    qmd_str = "---".join(template_split)
+    with open(f"docs/{mtype}/{nf_meta['name']}.qmd", "w") as outfile:
+        outfile.write(qmd_str)
 
 
 def get_yaml_globs(infileglob):
