4c51dc8ef5224591c2fd6240e6ff146d90cf0371
Kelly Sovacool <kelly.sovacool@nih.gov>
2023-11-22 08:58:24 -0500

    fix: escape backslashes for nf template script

--- modules/CCBR/custom/convertsicer/templates/convert-sicer.py
+++ modules/CCBR/custom/convertsicer/templates/convert-sicer.py
@@ -11,7 +11,7 @@ def main(infile, outbed, outpeak):
     outBroadPeak = [None] * len(intxt)
     outBed = [None] * len(intxt)
     for i in range(len(intxt)):
-        tmp = intxt[i].strip().split("\t")
+        tmp = intxt[i].strip().split("\\t")
         if len(tmp) == 8:
             # assuming that a p-value/q-value of 0 is super significant, -log10(1e-500)
             if tmp[5] == "0.0":
@@ -24,25 +24,25 @@ def main(infile, outbed, outpeak):
             else:
                 qval = str(-(math.log10(float(tmp[7]))))
                 qvalScore = str(int(-10 * math.log10(float(tmp[7]))))
-            outBroadPeak[i] = "\t".join(
+            outBroadPeak[i] = "\\t".join(
                 tmp[0:3] + ["Peak" + str(i + 1), tmp[3], ".", tmp[6], pval, qval]
             )
-            outBed[i] = "\t".join(tmp[0:3] + ["Peak" + str(i + 1), qvalScore])
+            outBed[i] = "\\t".join(tmp[0:3] + ["Peak" + str(i + 1), qvalScore])
         else:
             score = str(int(float(tmp[3])))
-            outBed[i] = "\t".join(tmp[0:3] + ["Peak" + str(i + 1), score])
+            outBed[i] = "\\t".join(tmp[0:3] + ["Peak" + str(i + 1), score])
     # output broadPeak columns: chrom, start, end, name, ChIP tag count, strand, fold-enrichment, -log10 p-value, -log10 q-value
     with open(outbed, "w") as g:
-        g.write("\n".join(outBed))
+        g.write("\\n".join(outBed))
     if outBroadPeak[0] != None:
         with open(outpeak, "w") as h:
-            h.write("\n".join(outBroadPeak))
+            h.write("\\n".join(outBroadPeak))
 
 
 def write_versions():
     with open("versions.yml", "w") as outfile:
-        outfile.write('"${task.process}":\\n')
-        outfile.write(f'  Python: "{platform.python_version()}"\\n')
+        outfile.write('"${task.process}":\\\\n')
+        outfile.write(f'  Python: "{platform.python_version()}"\\\\n')
 
 
 if __name__ == "__main__":
